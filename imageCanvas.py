0e2707d347e7e5d7bc77f85fa4eeafb14303dbbc
RussTorres <russtorres@gmail.com>
2017-03-14 13:31:19 -0700

    add render-python renderapi support, some PEP8

--- imageCanvas.py
+++ imageCanvas.py
@@ -1,10 +1,8 @@
-
 import cv2
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
-#from matplotlib.Artist import artist
 from matplotlib.mlab import dist_point_to_segment
-#from matplotlib.line import Lines2D
+
 
 class Polygon():
     def __init__(self):
@@ -20,13 +18,14 @@ class Polygon():
     def getWorldCoords(self):
         return self.worldCoords
 
+
 class imageCanvas():
-    def __init__(self,image):
+    def __init__(self, image):
         self.fname = image
         self.img = cv2.imread(self.fname)
         self.img_width = self.img.shape[1]
         self.img_height = self.img.shape[0]
-        #self.bounds = bounds
+        # self.bounds = bounds
         print self.img.shape
         self.point = ()
         self.polygon = []
@@ -41,9 +40,12 @@ class imageCanvas():
         ax = self.fig.add_subplot(111)
         ax.set_title(self.fname)
         plt.imshow(self.img)
-        self.__ID1 = self.fig.canvas.mpl_connect('motion_notify_event',self.__motion_notify_callback)
-        self.__ID2 = self.fig.canvas.mpl_connect('button_press_event',self.__button_press_callback)
-        self.__ID3 = self.fig.canvas.mpl_connect('close_event',self.__handle_close)
+        self.__ID1 = self.fig.canvas.mpl_connect(
+            'motion_notify_event', self.__motion_notify_callback)
+        self.__ID2 = self.fig.canvas.mpl_connect(
+            'button_press_event', self.__button_press_callback)
+        self.__ID3 = self.fig.canvas.mpl_connect(
+            'close_event', self.__handle_close)
         plt.show()
 
     def getCoord(self):
@@ -51,66 +53,73 @@ class imageCanvas():
         ax = self.fig.add_subplot(111)
         ax.set_title(self.fname)
         plt.imshow(self.img)
-        cid = self.fig.canvas.mpl_connect('button_press_event', self.__onclick__)
+        cid = self.fig.canvas.mpl_connect(
+            'button_press_event', self.__onclick__)
         plt.show()
         return self.point
 
-    def __onclick__(self,click):
-        self.point = (click.xdata,click.ydata)
+    def __onclick__(self, click):
+        self.point = (click.xdata, click.ydata)
         print self.point
         return self.point
 
-    def __motion_notify_callback(self,event):
+    def __motion_notify_callback(self, event):
         if event.inaxes:
             ax = event.inaxes
-            x,y = event.xdata, event.ydata
-            if (event.button == None or event.button == 1) and self.line != None:
-                self.line.set_data([self.previous_point[0],x],
-                                    [self.previous_point[1],y])
+            x, y = event.xdata, event.ydata
+            if ((event.button is None or event.button == 1) and
+                    self.line is not None):
+                self.line.set_data([self.previous_point[0], x],
+                                   [self.previous_point[1], y])
                 self.fig.canvas.draw()
 
-    def __button_press_callback(self,event):
+    def __button_press_callback(self, event):
         if event.inaxes:
-            x,y = event.xdata,event.ydata
+            x = event.xdata
+            y = event.ydata
             ax = event.inaxes
-            if event.button == 1 and event.dblclick == False: # pressed the left button and it is a single click
-                if self.line == None: # if there is no line, create a line
+            # single click of left button
+            if event.button == 1 and event.dblclick is False:
+                if self.line is None:  # if there is no line, create a line
                     self.newPoly = Polygon()
-                    self.line = plt.Line2D([x,x], [y,y], marker='o', color=self.newPoly.roicolor)
-                    self.previous_point = [x,y]
-                    self.newPoly.coords.append([x,y])
-                    self.newPoly.start_point = (x,y)
+                    self.line = plt.Line2D([x, x], [y, y], marker='o',
+                                           color=self.newPoly.roicolor)
+                    self.previous_point = [x, y]
+                    self.newPoly.coords.append([x, y])
+                    self.newPoly.start_point = (x, y)
                     ax.add_line(self.line)
                     self.fig.canvas.draw()
-                else: # if there is a line, then create a line segment (the polygon is already initialized)
+                else:  # if there is a line, then create a line segment (the polygon is already initialized)
                     self.line = plt.Line2D([self.previous_point[0], x],
                                            [self.previous_point[1], y],
-                                           marker='o', color=self.newPoly.roicolor)
-                    self.previous_point = [x,y]
-                    self.newPoly.coords.append([x,y])
+                                           marker='o',
+                                           color=self.newPoly.roicolor)
+                    self.previous_point = [x, y]
+                    self.newPoly.coords.append([x, y])
                     event.inaxes.add_line(self.line)
                     self.fig.canvas.draw()
-            elif ((event.button == 1 and even.dblclick == True) or
-                  (event.button == 3 and event.dblclick == False)) and self.line != None:
-                  # add the polgon to the list of polygons
-                  # complete the line connecting the start and the end point
-                  # display the image
-                  self.line.set_data([self.previous_point[0],
-                                      self.newPoly.start_point[0]],
-                                     [self.previous_point[1],
-                                      self.newPoly.start_point[1]])
-                  ax.add_line(self.line)
-                  self.fig.canvas.draw()
-                  self.line = None
-                  self.newPoly.end_point = self.previous_point
-                  # you can either opt to append the first point again at the end of newPoly.coords or just leave it
-                  self.polygon.append(self.newPoly)
+            elif (((event.button == 1 and even.dblclick is True) or
+                   (event.button == 3 and event.dblclick is False)) and
+                  self.line is not None):
+                # add the polgon to the list of polygons
+                # complete the line connecting the start and the end point
+                # display the image
+                self.line.set_data([self.previous_point[0],
+                                    self.newPoly.start_point[0]],
+                                   [self.previous_point[1],
+                                    self.newPoly.start_point[1]])
+                ax.add_line(self.line)
+                self.fig.canvas.draw()
+                self.line = None
+                self.newPoly.end_point = self.previous_point
+                # you can either opt to append the first point again at the end of newPoly.coords or just leave it
+                self.polygon.append(self.newPoly)
 
-    def __handle_close(self,event):
+    def __handle_close(self, event):
         print "Closing figure!"
-        #print self.polygon
+        # print self.polygon
 
-    def convertPolygonPointsToWorld(self,sectionBounds):
+    def convertPolygonPointsToWorld(self, sectionBounds):
         # converts the screen coordinates to world coordinates
         bounds_width = sectionBounds['maxX'] - sectionBounds['minX']
         bounds_height = sectionBounds['maxY'] - sectionBounds['minY']
@@ -121,11 +130,11 @@ class imageCanvas():
         # check if polygon points have been chosen
         coords = [[]]
         if len(self.polygon) > 0:
-            #for i in xrange(0,len(self.polygon)-1):
-            #    for j in xrange(0,len(self.polygon[i].coords)-1):
-            for i,poly in enumerate(self.polygon):
-                for x,y in poly.coords:
+            # for i in xrange(0,len(self.polygon)-1):
+            #     for j in xrange(0,len(self.polygon[i].coords)-1):
+            for i, poly in enumerate(self.polygon):
+                for x, y in poly.coords:
                     # convert the points to world coordinates
                     x = (x/scale) + sectionBounds['minX']
                     y = (y/scale) + sectionBounds['minY']
-                    self.polygon[i].worldCoords.append([x,y])
+                    self.polygon[i].worldCoords.append([x, y])
--- loadStack.py
+++ loadStack.py
@@ -1,17 +1,39 @@
-
-# Takes a render stack and a z-value for a section and reads in all the parameters required for locating tiles location in the downsampled images
-
-import os, sys, json
+#!/usr/bin/env python
+'''
+Takes a render stack and a z-value for a section and reads in all the
+    parameters required for locating tiles location in the downsampled images
+'''
+import os
+import sys
+import json
+import tempfile
 import urllib
 import matplotlib.pyplot as plt
 import matplotlib.path as mplpath
 import numpy as np
-import cv2, PIL
+import cv2
+import PIL
 from imageCanvas import *
-from stackPointConversion import stackPointConversion
+from stackPointConversion import *
+
+import renderapi
+
+render_connect = {
+    'host': 'http://em-131fs',
+    'port': 8080,
+    'owner': 'gayathri',
+    'project': 'EM_Phase1',
+    'client_scripts': '/data/em-131fs/code/render/render-ws-java-client/src/main/scripts/',
+    'memGB': '2G'}
 
 
 if __name__ == '__main__':
+    inputStack = 'Phase1RawData_AIBS'
+    targetStack = 'PolygonTest'
+
+    r = renderapi.connect(**render_connect)
+
+    '''
     stack = {}
     stack["baseUrl"] = "http://em-131fs:8080/render-ws/v1"
     stack["owner"] = "gayathri"
@@ -23,36 +45,44 @@ if __name__ == '__main__':
     targetstack["owner"] = "gayathri"
     targetstack["project"] = "EM_Phase1"
     targetstack["stackname"] = "PolygonTest"
+    '''
 
     downsampledImgPath = "/data/em-131fs/gayathri/downsampledSections"
     ext = "jpg"
     z = 2267
 
-    sp = stackPointConversion()
-
+    # sp = stackPointConversion()
+    tsjsons = []
     # Read the downscaled image of each section
     files = os.listdir(downsampledImgPath)
     for f in files:
-        f = os.path.join(downsampledImgPath,f)
+        f = os.path.join(downsampledImgPath, f)
         if (os.path.isfile(f) and f.endswith(ext)):
             # assumes that the downsampled images are named after their z value
             z = int(f[len(downsampledImgPath)+1:-4])
-            sectionId = str(z) + '.0'
-            print sectionId
+            if z > 2272:
+                break
+            # sectionId = str(z) + '.0'
+            # print sectionId
 
             # this is required to reupload sections with only selected tiles
-            tileSpecs = sp.getTileSpecs(stack,z)
-            #print tileSpecs[0]
+            # tileSpecs = sp.getTileSpecs(stack, z)
+            # print tileSpecs[0]
+            tilespecs = renderapi.tilespec.get_tile_specs_from_z(
+                inputStack, z, render=r)
 
             # get section bounds
-            secBounds = sp.getSectionBounds(stack,z)
+            # secBounds = sp.getSectionBounds(stack, z)
+            secbounds = renderapi.stack.get_bounds_from_z(
+                inputStack, z, render=r)
 
             # get section ID
-            sectionId = tileSpecs[0]["layout"]["sectionId"]
-            #print sectionId
+            # sectionId = tileSpecs[0]["layout"]["sectionId"]
+            sectionId = tilespecs[0].layout.sectionId
+            # print sectionId
 
             # get the bounds of all tiles in this section
-            tileBounds = sp.getTileBounds(stack, z)
+            # tileBounds = sp.getTileBounds(stack, z)
 
             # set up the image canvas to show the downscaled image and get the polygon roi
             canvasImage = imageCanvas(f)
@@ -61,17 +91,41 @@ if __name__ == '__main__':
             canvasImage.showImage()
 
             # convert the polygon points to world coordinates using section bounds
-            canvasImage.convertPolygonPointsToWorld(secBounds)
+            # canvasImage.convertPolygonPointsToWorld(secBounds)
+            canvasImage.convertPolygonPointsToWorld(secbounds)
 
             # find the tiles whose center falls within this polygon
-            print canvasImage.polygon[0].worldCoords
-            tileIDs = sp.selectTilesInsidePolygon(canvasImage.polygon[0].worldCoords, tileBounds)
-            print tileIDs
+            # print canvasImage.polygon[0].worldCoords
+            # tileIDs = sp.selectTilesInsidePolygon(
+            #     canvasImage.polygon[0].worldCoords, tileBounds)
+            # print tileIDs
+            try:
+                tilespecs_inside = select_tilespecs_inside_polygon(
+                    canvasImage.polygon[0].worldCoords, tilespecs)
+            except IndexError:
+                print "no polygon defined for {}!".format(sectionId)
+                tilespecs_inside = tilespecs
 
             # extract the tile specs corresponding to these tileIDs
-            tileSpecs = sp.getTileSpecsFromTileID(stack,tileIDs)
-            print tileSpecs
+            # tileSpecs = sp.getTileSpecsFromTileID(stack, tileIDs)
+            # print tileSpecs
 
+            # generate temporary json files
+            tempjson = tempfile.NamedTemporaryFile(
+                suffix='.json', mode='r', delete=False)
+            tempjson.close()
+            tsjson = tempjson.name
+            with open(tsjson, 'w') as f:
+                renderapi.utils.renderdump(tilespecs_inside, f)
+
+            tsjsons.append(tsjson)
+
+    renderapi.stack.create_stack(targetStack, render=r)
+    # upload tilespecs -- TODO add pool setup
+    renderapi.client.import_jsonfiles_parallel(
+        targetStack, tsjsons, poolsize=10, render=r)
+    for tsjson in tsjsons:
+        os.remove(tsjson)
 
 #    for f in files:
 #        f = os.path.join(downsampledImgPath, f)
